1cc8fbba0b5b3d73ad8b65daacff31138b65b726
Jayapritha <jayapritha9282@gmail.com>
2021-08-30 15:51:02 +0530

    added files

---
+++ classDeck/classroom/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Subject)
+admin.site.register(StudentAnswer)
+admin.site.register(TakenQuiz)
+admin.site.register(Student)
+admin.site.register(Answer)
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Quiz)
