9af231628d5fc8d49ac1e6e28db0ad4daebf6ad4
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-03-03 20:09:36 +0530

    Added Email field for student and teacher

--- classDeck/classroom/forms.py
+++ classDeck/classroom/forms.py
@@ -10,6 +10,7 @@ from classroom.models import (Answer, Question, Student, StudentAnswer,
 class TeacherSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
+        fields=['username','email','password1','password2']
 
     def save(self, commit=True):
         user = super().save(commit=False)
