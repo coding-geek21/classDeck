08dfaeabc2feae9da63c9f38d6eef9446e6825b4
Rahul0700 <antorahul070@gmail.com>
2022-03-08 16:01:18 +0530

    Issue #46 Forgot password feature

---
+++ classDeck/classroom/views/reset_password.py
@@ -0,0 +1,14 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'reset_password/password_reset.html'
+    email_template_name = 'reset_password/password_reset_email.html'
+    subject_template_name = 'reset_password/password_reset_subject.txt'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('home')
