d9291d6ded8140d0c5bc4d7f579128273457d3e2
Ashsenior <ashutoshjunior@gmail.com>
2022-04-29 00:10:05 +0530

    added the timer

--- classDeck/classroom/models.py
+++ classDeck/classroom/models.py
@@ -26,6 +26,7 @@ class Quiz(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quizzes')
     name = models.CharField(max_length=255)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='quizzes')
+    duration = models.TimeField(null=True)
 
     def __str__(self):
         return self.name
